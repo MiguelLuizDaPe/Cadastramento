d451af90d011e2ddc2d8f399f91785307241186b
miguel <drporrola@gmail.com>
2023-05-16 17:35:07 -0300

    foi

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Cadastramento // Note: actual namespace depends on the project name.
     internal class Program{
         static void Main(string[] args){
             Cliente c = new Cliente(1, "pedro", "aaa", "sss", "ddd");
-            Console.WriteLine(c.nome);
+            Console.WriteLine("pinto");
         }
     }
 }
