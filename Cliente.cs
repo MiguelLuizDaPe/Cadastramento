ecbefb3408fab7ae24a426b489c0ded3989d048e
Miguel Luiz <111905998+MiguelLuizDaPe@users.noreply.github.com>
2023-05-17 15:32:10 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,23 +1,39 @@
 using System;
 
-namespace Cadastramento{
-    class Cliente{
-        //tudo public pq não tem motivo pra ser private
-        public int id;
-        public string nome;
-        public string endereco;
-        public string telefone;
-        public string email;
+namespace Cadastramento
+{
+    class Cliente
+    {
+        private int id;
+        public int getId() { return this.id; }
+        public void setId(int id) { this.id = id; }
 
-        public Cliente(){}
-        public Cliente(int id, string nome, string endereco, string telefone, string email){
+        private string nome;
+        public string getNome() { return this.nome; }
+        public void setNome(string nome) { this.nome = nome; }
+
+        private string email;
+        public string getEmail() { return this.email; }
+        public void setEmail(string email) { this.email = email; }
+
+        private string endereco;
+        public string getEndereco() { return this.endereco; }
+        public void setEndereco(string endereco) { this.endereco = endereco; }
+
+        private string telefone;
+        public string getTelefone() { return this.telefone; }
+        public void setTelefone(string telefone) { this.telefone = telefone; }
+
+        public Cliente() { }
+        public Cliente(int id, string nome, string endereco, string telefone, string email)
+        {
             this.id = id;
             this.nome = nome;
             this.email = email;
             this.endereco = endereco;
             this.telefone = telefone;
         }
-        
+
     }
 
 }
